5f0e91d9182f6cefa2c775fb100c0e04dbb5293e
Muhammad Alif <43428700+maximustylus@users.noreply.github.com>
2026-04-06 13:01:15 +0800

    Update main.jsx
    Signed-off-by: Muhammad Alif <43428700+maximustylus@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './index.css'
 import './style.css'
+
+// THIS WAS MISSING:
 import { NexusProvider } from './context/NexusContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
